cc599441c614c4d7aa04541866156a0524746504
sdragos1 <dragos.suceveanu@epitech.eu>
2025-10-22 14:55:34 +0800

    feat: add task2

--- spacenews/task2/main.py
+++ spacenews/task2/main.py
@@ -4,7 +4,7 @@ from pyspark.sql.functions import explode, split, regexp_replace, lower, col, co
 CLEAN_WORD_REGEX = "[^a-z0-9\\s']"
 HDFS_URI = "hdfs://192.168.0.12:9000"
 
-spark = SparkSession.builder.appName("SpaceNews - Task 1").master(
+spark = SparkSession.builder.appName("SpaceNews - Task 2").master(
     "spark://192.168.0.2:7077").getOrCreate()
 
 df = spark.read.csv(f"{HDFS_URI}/data/spacenews.csv",
@@ -23,7 +23,7 @@ words_df = df.select(
     to_date(col("date"), "MMMM d, yyyy").alias("date"),
     explode(
         split(
-            regexp_replace(lower(col("title")), CLEAN_WORD_REGEX, " "),
+            regexp_replace(lower(col("content")), CLEAN_WORD_REGEX, " "),
             "\\s+"
         )
     ).alias("word")
@@ -34,13 +34,13 @@ word_counts_total = words_df.drop("date").groupBy("word") \
     .orderBy(desc("count"))
 word_counts_total.show(50, truncate=False)
 word_counts_total.coalesce(1).write.mode("overwrite") \
-    .csv(f"{HDFS_URI}/output/spacenews/title_word_count_total", header=False, sep=" ")
+    .csv(f"{HDFS_URI}/output/spacenews/content_word_count_total", header=False, sep=" ")
 
 word_counts_per_day = words_df.groupBy("date", "word") \
     .agg(count("*").alias("count")) \
     .orderBy(col("date").desc(), desc("count"))
 word_counts_per_day.show(50, truncate=False)
 word_counts_per_day.coalesce(1).write.mode("overwrite") \
-    .csv(f"{HDFS_URI}/output/spacenews/title_word_count_per_day", header=False, sep=" ")
+    .csv(f"{HDFS_URI}/output/spacenews/content_word_count_per_day", header=False, sep=" ")
 
 spark.stop()
